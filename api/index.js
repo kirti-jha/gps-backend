5c56f3835204480afe01569f2879f99adf802c8c
KIRTIJHA-DEVELOPER <KIRTIJHA@ABHEEPAY.COM>
2026-09-03 18:50:56 +0530

    fix(vercel): export module.exports = app.default || app for Vercel serverless runtime

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("../src/index"));
-exports.default = index_1.default;
+const app = require('../dist/index');
+
+module.exports = app.default || app;
